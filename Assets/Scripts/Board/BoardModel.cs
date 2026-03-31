610bc71e27faff19d03b7a96c2bd066c40f4a28d
Vivian Horing <vivianhoring@gmail.com>
2026-03-31 02:04:11 -0300

    Sprites fixes

--- Assets/Scripts/Board/BoardModel.cs
+++
@@ -1,6 +0,0 @@
-public class BoardModel
-{
-    //Representação matemática do board
-    //Guarda estado do board
-    //Possui lógica do jogo: reveal flood fill
-}
--- Assets/Scripts/UI/MainMenuController.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class MainMenuController : MonoBehaviour
-{
-    //Controla o Main Menu
-}
